972686726ddc0204dd8a3135a4e5d04d869bb1ca
Etienne BAUDOUX <ebaudoux@outlook.com>
2020-07-03 10:56:37 -0700

    Updated assembly info

--- Windows/Def/PaZword.Api/Properties/AssemblyInfo.cs
+++ Windows/Def/PaZword.Api/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2020.7.1.3")]
-[assembly: AssemblyFileVersion("2020.7.1.3")]
+[assembly: AssemblyVersion("2020.7.3.2")]
+[assembly: AssemblyFileVersion("2020.7.3.2")]
 
