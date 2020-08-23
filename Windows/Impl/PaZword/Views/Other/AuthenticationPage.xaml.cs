feabbf54ef83c105b4f95a0c8428c95e1220431b
Etienne BAUDOUX <ebaudoux@outlook.com>
2020-08-23 10:22:37 -0700

    Fix #21 - Authentication UI wasn't getting proper focus.

--- Windows/Impl/PaZword/Views/Other/AuthenticationPage.xaml.cs
+++ Windows/Impl/PaZword/Views/Other/AuthenticationPage.xaml.cs
@@ -1,4 +1,9 @@
 ﻿using PaZword.ViewModels.Other;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace PaZword.Views.Other
@@ -21,6 +26,45 @@ namespace PaZword.Views.Other
             InitializeComponent();
 
             ViewModel.Initialize();
+
+            RecoveryKeyStackPanel.RegisterPropertyChangedCallback(UIElement.VisibilityProperty, VisibilityChanged);
+            RetryWindowsHelloStackPanel.RegisterPropertyChangedCallback(UIElement.VisibilityProperty, VisibilityChanged);
+            TwoFactorAuthenticationEmailAddressStackPanel.RegisterPropertyChangedCallback(UIElement.VisibilityProperty, VisibilityChanged);
+            TwoFactorAuthenticationGrid.RegisterPropertyChangedCallback(UIElement.VisibilityProperty, VisibilityChanged);
+        }
+
+        private void VisibilityChanged(DependencyObject sender, DependencyProperty dp)
+        {
+            if (sender is Panel control
+                && control.Visibility == Visibility.Visible)
+            {
+                // Slightly delay setting focus
+                Task.Factory.StartNew(async () =>
+                {
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.Low, () =>
+                    {
+                        if (control == RecoveryKeyStackPanel)
+                        {
+                            RecoveryKeyTextBox.Focus(FocusState.Keyboard);
+                        }
+                        else if (control == RetryWindowsHelloStackPanel)
+                        {
+                            RetryWindowsHelloAuthenticationButton.Focus(FocusState.Keyboard);
+                        }
+                        else if (control == TwoFactorAuthenticationGrid)
+                        {
+                            TwoFactorAuthenticationTextBox.Focus(FocusState.Keyboard);
+                        }
+                        else if (control == TwoFactorAuthenticationEmailAddressStackPanel)
+                        {
+                            TwoFactorAuthenticationEmailAddressTextBox.Focus(FocusState.Keyboard);
+                        }
+                    });
+                },
+                CancellationToken.None,
+                TaskCreationOptions.None,
+                TaskScheduler.Default);
+            }
         }
     }
 }
